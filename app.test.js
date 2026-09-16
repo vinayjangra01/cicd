f2951195eee81f0cf3c932703747c73b578792a5
vinay kumar <vinay.kumar@gmail.com>
2026-09-16 23:15:17 +0530

    Initial commit

---
+++ app.test.js
@@ -0,0 +1,13 @@
+const request = require('supertest');
+const { describe, it, expect } = require('@jest/globals');
+const app = require('./app');
+
+describe('GET /', () => {
+  it(`should return 'Works on my machine.'`, async () => {
+    const res = await request(app)
+      .get('/')
+      .expect(200);
+
+    expect(res.text).toBe('Works on my machine.');
+  });
+});
