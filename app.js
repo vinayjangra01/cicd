e0c762007f204b44219f1c67eb523dcb55887e23
vinay kumar <vinay.kumar@gmail.com>
2026-09-18 16:09:09 +0530

    chore: add prettier configuration and github actions ci workflow

--- app.js
+++ app.js
@@ -1,31 +1,12 @@
 const express = require('express');
 const app = express();
 
-console.log("bye world")
+console.log('bye world');
 
 app.get('/', (req, res) => {
   res.send('Works on my machine.');
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-console.log("bye world")
+console.log('bye world');
 
 module.exports = app;
